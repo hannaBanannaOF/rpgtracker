020f4b1c4a6cadca95c7021da1f56f13959d7ffc
Lucas Westphal <westphallucas1@outlook.com>
2022-02-08 10:17:34 -0300

    AJuste https-only

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
+
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
@@ -5,6 +14,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 require('dotenv').config()
 
+app.use(requireHTTPS);
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
